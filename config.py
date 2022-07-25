2dd5dced47f8e4bcc359b0ba50f6b8444d401908
Shawkh Ibne Rashid <shawkh9007@gmail.com>
2022-07-24 20:27:01 -0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flaskext.mysql import MySQL
-
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'b5bd14e1d764b3'
-app.config['MYSQL_DATABASE_PASSWORD'] = '92ab938d'
-app.config['MYSQL_DATABASE_DB'] = 'heroku_6e943b69a8f614b'
-app.config['MYSQL_DATABASE_HOST'] = 'us-cdbr-east-06.cleardb.net'
-mysql.init_app(app)
